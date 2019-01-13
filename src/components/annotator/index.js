b176b89608a478185ddf1ceab23a32470b63b861
chamathabeysinghe <abeysinghechamath@gmail.com>
2019-01-13 19:23:18 +0530

    Minor changes

--- src/components/annotator/index.js
+++ src/components/annotator/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Grid, Button, List, Icon} from 'semantic-ui-react'
+import {Grid, Button, List, Message, Dimmer, Loader} from 'semantic-ui-react'
 import {Player} from 'video-react'
 import axios from 'axios'
 import AnnotatorForm from './annotatorform'
@@ -10,6 +10,7 @@ class Annotator extends Component {
   constructor (props) {
     super (props)
     this.state = {
+      loading:false,
       video: undefined
     }
   }
@@ -19,34 +20,60 @@ class Annotator extends Component {
     annotation['video_url'] = video.video_url
     annotation['video_id'] = video._id
     var form = {video, annotation}
-    console.log(annotation)
+    this.setState({
+      loading: true
+    })
+
     axios.post('http://localhost:1234/api/annotation', form)
       .then(function(response){
         console.log(response)
+        this.setState({
+          loading: false
+        })
       })
       .catch(function (err) {
         console.log(err)
+        this.setState({
+          loading: false
+        })
       })
   }
 
   componentWillMount () {
     var self = this
+    this.setState({
+      loading: true
+    })
     axios.get('http://localhost:1234/api/video')
       .then(function (response) {
         console.log(response.data.result)
         var video = response.data.result
         self.setState({
+          loading: false,
           video: video
         })
       })
       .catch(function (error) {
+        self.setState({
+          loading: false,
+        })
         console.log(error)
       })
   }
 
   render() {
+    if (this.state.video === undefined && !this.state.loading) {
+      return (
+        <Message negative>
+          <Message.Header>Server has no videos right now</Message.Header>
+        </Message>
+      )
+    }
     return (
       <div>
+        <Dimmer active={this.state.loading}>
+          <Loader />
+        </Dimmer>
         <Grid>
           <Grid.Row columns={2}>
             <Grid.Column width={8}>
