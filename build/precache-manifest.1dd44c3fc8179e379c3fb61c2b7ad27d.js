169a0ed80f9a05eb8a5066d7e773bcc7b4c87198
chamathabeysinghe <abeysinghechamath@gmail.com>
2019-01-14 18:35:56 +0530

    Build

---
+++ build/precache-manifest.1dd44c3fc8179e379c3fb61c2b7ad27d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50a58b77e35431338153",
+    "url": "/static/js/main.50a58b77.chunk.js"
+  },
+  {
+    "revision": "3a90a3b95f3b0da9680c",
+    "url": "/static/js/1.3a90a3b9.chunk.js"
+  },
+  {
+    "revision": "50a58b77e35431338153",
+    "url": "/static/css/main.aaeff4e8.chunk.css"
+  },
+  {
+    "revision": "3a90a3b95f3b0da9680c",
+    "url": "/static/css/1.f140487d.chunk.css"
+  },
+  {
+    "revision": "ca4637f9e771fce40023dc376f2e02a3",
+    "url": "/index.html"
+  }
+];
