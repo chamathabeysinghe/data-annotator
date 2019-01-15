bee02a35d5b3223abf978da72bc80827c53a66a8
chamathabeysinghe <abeysinghechamath@gmail.com>
2019-01-15 19:10:30 +0530

    Bulk api endpoint

---
+++ build/precache-manifest.d37f04a53e4ef93a91c0383c646ac827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13ef0437c3c2daca51a5",
+    "url": "/static/js/main.13ef0437.chunk.js"
+  },
+  {
+    "revision": "3a90a3b95f3b0da9680c",
+    "url": "/static/js/1.3a90a3b9.chunk.js"
+  },
+  {
+    "revision": "13ef0437c3c2daca51a5",
+    "url": "/static/css/main.aaeff4e8.chunk.css"
+  },
+  {
+    "revision": "3a90a3b95f3b0da9680c",
+    "url": "/static/css/1.f140487d.chunk.css"
+  },
+  {
+    "revision": "974b1e448b0859c7d7732248cba8e19c",
+    "url": "/index.html"
+  }
+];
