8b0559dacc748109b4559477226c0470410e7d31
chamathabeysinghe <abeysinghechamath@gmail.com>
2019-02-05 10:35:27 +0530

    New build

--- build/precache-manifest.acf9e8c08bfff8bd5c023fe3f072df57.js
+++ build/precache-manifest.acf9e8c08bfff8bd5c023fe3f072df57.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "13ef0437c3c2daca51a5",
-    "url": "/static/js/main.13ef0437.chunk.js"
+    "revision": "ee0b05808f9ca8382613",
+    "url": "/static/js/main.ee0b0580.chunk.js"
   },
   {
     "revision": "3a90a3b95f3b0da9680c",
     "url": "/static/js/1.3a90a3b9.chunk.js"
   },
   {
-    "revision": "13ef0437c3c2daca51a5",
+    "revision": "ee0b05808f9ca8382613",
     "url": "/static/css/main.aaeff4e8.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.f140487d.chunk.css"
   },
   {
-    "revision": "974b1e448b0859c7d7732248cba8e19c",
+    "revision": "4c0c8841376aa0b7f8eff15e7c18b88a",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d37f04a53e4ef93a91c0383c646ac827.js"
+  "/precache-manifest.acf9e8c08bfff8bd5c023fe3f072df57.js"
 );
 
 workbox.clientsClaim();
