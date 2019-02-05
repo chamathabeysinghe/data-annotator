1d07ffbabe991d04cb8b9e21cb124934edc00e43
chamathabeysinghe <abeysinghechamath@gmail.com>
2019-02-05 11:02:38 +0530

    Build new app

---
+++ build/precache-manifest.755e5f613e94e48021e69ce29441c5fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a452f65a856c554614ba",
+    "url": "/static/js/main.a452f65a.chunk.js"
+  },
+  {
+    "revision": "3a90a3b95f3b0da9680c",
+    "url": "/static/js/1.3a90a3b9.chunk.js"
+  },
+  {
+    "revision": "a452f65a856c554614ba",
+    "url": "/static/css/main.aaeff4e8.chunk.css"
+  },
+  {
+    "revision": "3a90a3b95f3b0da9680c",
+    "url": "/static/css/1.f140487d.chunk.css"
+  },
+  {
+    "revision": "6ea7b9bb471e60ca8e4d989bc47e30be",
+    "url": "/index.html"
+  }
+];
